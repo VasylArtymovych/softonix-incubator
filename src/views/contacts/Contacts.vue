8c310616d60cd965883ebfe3a7d6faa282f33e0e
VasylArtymovych <art777vasss@gmail.com>
2023-05-01 20:47:00 +0300

    add transition to Contacts tabs

--- src/views/contacts/Contacts.vue
+++ src/views/contacts/Contacts.vue
@@ -1,22 +1,42 @@
 <template>
   <el-tabs v-model="activeTab" type="border-card" @tab-click="handleTabClick">
-    <el-tab-pane label="Contacts list" name="first">
-      <ContactsList />
-    </el-tab-pane>
+    <Transition mode="out-in">
+      <el-tab-pane label="Contacts list" name="first">
+        <ContactsList />
+      </el-tab-pane>
+    </Transition>
 
-    <el-tab-pane label="Contacts table" name="second">
-      <ContactsTable />
-    </el-tab-pane>
+    <Transition mode="out-in">
+      <el-tab-pane label="Contacts table" name="second">
+        <ContactsTable />
+      </el-tab-pane>
+    </Transition>
   </el-tabs>
 </template>
 
 <script lang="ts" setup>
 import type { TabsPaneContext } from 'element-plus'
 
-const activeTab = ref<TActiveTab>(localStorage.getItem('currTab') as TActiveTab || 'first')
+const activeTab = ref<TActiveTab>(localStorage.getItem('activeTab') as TActiveTab || 'first')
 
 const handleTabClick = (tab: TabsPaneContext) => {
   activeTab.value = tab.props.name as TActiveTab
-  localStorage.setItem('currTab', `${tab.props.name}`)
+  localStorage.setItem('activeTab', `${tab.props.name}`)
 }
 </script>
+
+<style lang="scss" scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-active {
+  transition-delay: 0.55s;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
